0de6c5414942164fcb4b99666da3227e3c2cc182
Scott Bromander <scott@primeacademy.io>
2016-02-11 06:47:47 -0600

    update

--- server/app.js
+++ server/app.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//APP SETTINGS
+mongoose.connect('mongodb://localhost:27017/iota_cats');
 
 app.set('port', 5000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
+//SCHEME
+var Cat = mongoose.model('Cat', {name: String});
 
 //ROUTE FUNCTIONALITY
+app.post('/cat', function(req,res){
+    console.log("ITS ALIVE! ", req.body);
+
+    var kitty = new Cat({
+        name: req.body.catName
+    });
+
+    kitty.save(function(err, gremlins){
+        if(err){
+            console.log(err);
+        }
+
+        res.send(gremlins);
+    });
+});
+
+app.get("/cat", function(req,res){
+    Cat.find({}).exec(function(err, gremlins){
+        if(err){
+            console.log(err);
+        }
+
+        res.send(gremlins);
+    });
+});
+
 app.get('/*', function(req, res){
     console.log("Here is the request: " , req.params);
     var file = req.params[0] || "/views/index.html";
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,1 +1,43 @@
-console.log("Hello");
+$(document).ready(function(){
+    $('#nameForm').on('submit', function(event){
+        event.preventDefault();
+
+        var values = {};
+
+        $.each($('#nameForm').serializeArray(), function(i, field){
+            values[field.name] = field.value;
+            //values.catName
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "/cat",
+            data: values,
+            success: function(data){
+                console.log(data);
+                fetchCats();
+            }
+        });
+    });
+
+    fetchCats();
+});
+
+function fetchCats(){
+    $.ajax({
+        type: "GET",
+        url: "/cat",
+        success: function(data){
+
+            refreshDom(data);
+        }
+    })
+}
+
+function refreshDom(array){
+    $("#catContainer").empty();
+
+    for(var i = 0; i < array.length; i++){
+        $("#catContainer").append("<p>" + array[i].name + "</p>")
+    }
+}
